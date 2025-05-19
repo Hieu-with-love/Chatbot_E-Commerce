ebd8c8d816c72d783d51cd1e990274d9efddfd8d
devzeus <hieu01bdvn@gmail.com>
2025-05-19 10:28:46 +0700

    Add Api: Update prefilter and write register api feature

--- chatbot_spring/src/main/java/android/hcmute/edu/vn/chatbot_spring/service/impl/AuthServiceImpl.java
+++ chatbot_spring/src/main/java/android/hcmute/edu/vn/chatbot_spring/service/impl/AuthServiceImpl.java
@@ -1,4 +1,47 @@
 package android.hcmute.edu.vn.chatbot_spring.service.impl;
 
-public class AuthServiceImpl {
+import android.hcmute.edu.vn.chatbot_spring.dto.request.RegisterRequest;
+import android.hcmute.edu.vn.chatbot_spring.enums.USER_ROLE;
+import android.hcmute.edu.vn.chatbot_spring.model.User;
+import android.hcmute.edu.vn.chatbot_spring.repository.UserRepository;
+import android.hcmute.edu.vn.chatbot_spring.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean login(String username, String password) {
+        return false;
+    }
+
+    @Override
+    public boolean register(RegisterRequest req) {
+        try{
+            USER_ROLE role;
+            if (req.getRole()==null){
+                role = USER_ROLE.USER;
+            }else {
+                role = USER_ROLE.valueOf(req.getRole());
+            }
+
+            User user = new User();
+            user.setEmail(req.getEmail());
+            user.setFullName(req.getFullName());
+            user.setPhone(req.getPhone());
+            user.setPassword(passwordEncoder.encode(req.getPassword()));
+            user.setRole(role.getRole());
+
+            userRepository.save(user);
+
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
